a6a0a0bf22cb0625f5e4b676dfa5e3aae5f632b4
Anuj Jadhav <87408381+AnujDev1000@users.noreply.github.com>
2023-01-05 11:52:51 +0530

    Delete backend directory

--- backend/models/customerModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const customerModel = mongoose.Schema({
-    name: { type: String, required: true, default: "none" },
-    phone: { type: String, default: "" },
-    products: []
-}, { timestamps: true })
-
-module.exports = mongoose.model("customers", customerModel)
--- backend/models/stockModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const stockModel = mongoose.Schema({
-    availableStock: { type: Number, required: true,},
-    product: { type: {}, required: true, default: {} },
-}, { timestamps: true })
-
-module.exports = mongoose.model("stocks", stockModel)
